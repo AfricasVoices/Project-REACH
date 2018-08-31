c285eb20608f4db5e4bc26e26c578ea421059834
Alexander Simpson <as2388@cam.ac.uk>
2018-08-31 11:18:43 +0300

    Tag data from test contacts as 'test_runs'

--- run_scripts/01_fetch_messages.sh
+++ run_scripts/01_fetch_messages.sh
@@ -14,6 +14,8 @@ RP_SERVER=$3
 RP_TOKEN=$4
 DATA_ROOT=$5
 
+TEST_CONTACTS_PATH="$(pwd)/test_contacts.json"
+
 cd "$RP_DIR"
 
 mkdir -p "$DATA_ROOT/01 Raw Messages"
@@ -26,6 +28,7 @@ for SHOW in ${SHOWS[@]}
 do
     echo "Exporting show $SHOW"
 
-    sh docker-run.sh "$RP_SERVER" "$RP_TOKEN" "$SHOW" "$USER" all \
+    sh docker-run.sh --flow-name "$SHOW" --test-contacts-path "$TEST_CONTACTS_PATH" \
+        "$RP_SERVER" "$RP_TOKEN" "$USER" all \
         "$DATA_ROOT/00 UUIDs/phone_uuids.json" "$DATA_ROOT/01 Raw Messages/$SHOW.json"
 done
---
+++ run_scripts/checkout_rapid_pro_tools.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Usage: sh checkout_rapid_pro_tools.sh <rapid-pro-dir>"
+    echo "Ensures that a copy of the RapidProTools project exists in  'rapid-pro-root' by cloning/fetching as, "
+    echo "necessary, and checking-out the version needed by this project"
+    exit
+fi
+
+RAPID_PRO_DIR="$1"
+
+RAPID_PRO_REPO="https://github.com/AfricasVoices/RapidProTools.git"
+TAG="43d8b4d49a3be5935069d48db5250f33c81bd924"
+
+mkdir -p "$RAPID_PRO_DIR"
+cd "$RAPID_PRO_DIR"
+
+# If the RAPID_PRO_DIR does not contain a git repository, clone the rapid pro repo
+if ! [ -d .git ]; then
+    git clone "$RAPID_PRO_REPO" .
+fi
+
+# Check that this repository is connected to the correct remote.
+# (this ensures we are in the correct repository and can fetch new changes later if we need to)
+if [ $(git config --get remote.origin.url) != "$RAPID_PRO_REPO" ]; then
+    echo "Error: Git repository in RAPID_PRO_ROOT does not have its origin set to $RAPID_PRO_REPO"
+    exit 1
+fi
+
+# If the provided tag wasn't found, run git fetch in case the remote has been updated
+if ! [ $(git tag -l "$TAG") ]; then
+    git fetch --tags
+fi
+
+# Checkout the requested tag
+git checkout -q "$TAG"
