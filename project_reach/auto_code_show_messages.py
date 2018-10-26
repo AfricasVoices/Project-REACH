feb0705efcf57d1bdd630fc231f5f53baf1b37a8
Alexander Simpson <as2388@cam.ac.uk>
2018-10-26 12:53:26 +0100

    Rename show_messages -> data

--- project_reach/auto_code_show_messages.py
+++ project_reach/auto_code_show_messages.py
@@ -13,18 +13,18 @@ from dateutil.parser import isoparse
 
 class AutoCodeShowMessages(object):
     @staticmethod
-    def auto_code_show_messages(user, show_messages, icr_output_path, coda_output_path, prev_coda_path):
+    def auto_code_show_messages(user, data, icr_output_path, coda_output_path, prev_coda_path):
         variable_name = "S07E01_Humanitarian_Priorities"
         flow_name = "esc4jmcna_activation"
 
         ICR_MESSAGES_COUNT = 200
 
         # Filter out test messages sent by AVF.
-        show_messages = [td for td in show_messages if not td.get("test_run", False)]
+        data = [td for td in data if not td.get("test_run", False)]
 
         # Filter for runs which contain a response to this week's question.
         show_message_key = "{} (Text) - {}".format(variable_name, flow_name)
-        show_messages = [td for td in show_messages if show_message_key in td]
+        data = [td for td in data if show_message_key in td]
 
         # Convert date/time of messages to EAT and filter out messages sent outwith the project run period
         utc_key = "{} (Time) - {}".format(variable_name, flow_name)
@@ -32,7 +32,7 @@ class AutoCodeShowMessages(object):
         inside_time_window = []
         START_TIME = isoparse("2018-09-09T00+03:00")
         END_TIME = isoparse("2018-09-17T00+03:00")
-        for td in show_messages:
+        for td in data:
             utc_time = isoparse(td[utc_key])
             eat_time = utc_time.astimezone(pytz.timezone("Africa/Nairobi")).isoformat()
 
@@ -46,21 +46,21 @@ class AutoCodeShowMessages(object):
             else:
                 print("Dropping: {}".format(utc_time))
 
-        print("{}:{} Dropped as outside time/Total".format(len(show_messages) - len(inside_time_window),
-                                                           len(show_messages)))
-        show_messages = inside_time_window
+        print("{}:{} Dropped as outside time/Total".format(len(data) - len(inside_time_window),
+                                                           len(data)))
+        data = inside_time_window
 
         # Filter out messages containing only noise
         print("Messages classified as noise:")
         not_noise = []
-        for td in show_messages:
+        for td in data:
             if somali.DemographicCleaner.is_noise(td[show_message_key], min_length=20):
                 print("Dropping: {}".format(td[show_message_key]))
                 td.append_data({"noise": "true"}, Metadata(user, Metadata.get_call_location(), time.time()))
             else:
                 not_noise.append(td)
 
-        print("{}:{} Dropped as noise/Total".format(len(show_messages) - len(not_noise), len(show_messages)))
+        print("{}:{} Dropped as noise/Total".format(len(data) - len(not_noise), len(data)))
 
         # Output messages which aren't noise to Coda
         IOUtils.ensure_dirs_exist_for_file(coda_output_path)
@@ -87,4 +87,4 @@ class AutoCodeShowMessages(object):
         with open(icr_output_path, "w") as f:
             TracedDataCSVIO.export_traced_data_iterable_to_csv(icr_messages, f, headers=[run_id_key, raw_text_key])
 
-        return show_messages
+        return data
