a57d116bc6bade8a09eabbc7198c19a6898b85c6
Alexander Simpson <as2388@cam.ac.uk>
2018-11-01 17:15:59 +0000

    Modify functions in AnalysisKeys to accept iterables of TracedData instead of just TracedData
    i.e. pushes the loops in analysis_file.py into AnalysisKeys

--- project_reach/lib/analysis_keys.py
+++ project_reach/lib/analysis_keys.py
@@ -18,51 +18,54 @@ class AnalysisKeys(object):
         return isoparse(td["created_on"]).astimezone(pytz.timezone("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M")
 
     @staticmethod
-    def set_yes_no_matrix_keys(user, td, show_keys, coded_shows_prefix, radio_q_prefix):
-        matrix_d = dict()
+    def set_yes_no_matrix_keys(user, data, show_keys, coded_shows_prefix, radio_q_prefix):
+        for td in data:
+            matrix_d = dict()
 
-        yes_no_key = coded_shows_prefix + "_yes_no"
-        yes_no = td[yes_no_key]
-        matrix_d[radio_q_prefix] = yes_no
+            yes_no_key = coded_shows_prefix + "_yes_no"
+            yes_no = td[yes_no_key]
+            matrix_d[radio_q_prefix] = yes_no
 
-        for key in td:
-            if key.startswith(coded_shows_prefix) and key != yes_no_key:
-                yes_prefix = radio_q_prefix + "_yes"
-                no_prefix = radio_q_prefix + "_no"
+            for key in td:
+                if key.startswith(coded_shows_prefix) and key != yes_no_key:
+                    yes_prefix = radio_q_prefix + "_yes"
+                    no_prefix = radio_q_prefix + "_no"
 
-                code_yes_key = key.replace(coded_shows_prefix, yes_prefix)
-                code_no_key = key.replace(coded_shows_prefix, no_prefix)
-                show_keys.update({code_yes_key, code_no_key})
+                    code_yes_key = key.replace(coded_shows_prefix, yes_prefix)
+                    code_no_key = key.replace(coded_shows_prefix, no_prefix)
+                    show_keys.update({code_yes_key, code_no_key})
 
-                matrix_d[code_yes_key] = td[key] if yes_no == Codes.YES else "0"
-                matrix_d[code_no_key] = td[key] if yes_no == Codes.NO else "0"
+                    matrix_d[code_yes_key] = td[key] if yes_no == Codes.YES else "0"
+                    matrix_d[code_no_key] = td[key] if yes_no == Codes.NO else "0"
 
-        td.append_data(matrix_d, Metadata(user, Metadata.get_call_location(), time.time()))
-
-    @staticmethod
-    def set_matrix_keys(user, td, show_keys, coded_shows_prefix, radio_q_prefix):
-        matrix_d = dict()
-
-        stopped = td.get("{}_{}".format(coded_shows_prefix, Codes.STOP)) == "1"
-
-        for output_key in td:
-            if output_key.startswith(coded_shows_prefix):
-                code_key = output_key.replace(coded_shows_prefix, radio_q_prefix)
-
-                if code_key.endswith(Codes.STOP):
-                    continue
-
-                show_keys.add(code_key)
-                if stopped:
-                    matrix_d[code_key] = Codes.STOP
-                else:
-                    matrix_d[code_key] = td[output_key]
-
-        td.append_data(matrix_d, Metadata(user, Metadata.get_call_location(), time.time()))
+            td.append_data(matrix_d, Metadata(user, Metadata.get_call_location(), time.time()))
 
     @staticmethod
-    def set_analysis_keys(user, td, key_map):
-        td.append_data(
-            {new_key: td[old_key] for new_key, old_key in key_map.items()},
-            Metadata(user, Metadata.get_call_location(), time.time())
-        )
+    def set_matrix_keys(user, data, show_keys, coded_shows_prefix, radio_q_prefix):
+        for td in data:
+            matrix_d = dict()
+
+            stopped = td.get("{}_{}".format(coded_shows_prefix, Codes.STOP)) == "1"
+
+            for output_key in td:
+                if output_key.startswith(coded_shows_prefix):
+                    code_key = output_key.replace(coded_shows_prefix, radio_q_prefix)
+
+                    if code_key.endswith(Codes.STOP):
+                        continue
+
+                    show_keys.add(code_key)
+                    if stopped:
+                        matrix_d[code_key] = Codes.STOP
+                    else:
+                        matrix_d[code_key] = td[output_key]
+
+            td.append_data(matrix_d, Metadata(user, Metadata.get_call_location(), time.time()))
+
+    @staticmethod
+    def set_analysis_keys(user, data, key_map):
+        for td in data:
+            td.append_data(
+                {new_key: td[old_key] for new_key, old_key in key_map.items()},
+                Metadata(user, Metadata.get_call_location(), time.time())
+            )
